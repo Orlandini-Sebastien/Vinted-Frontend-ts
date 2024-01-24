93651b6b52da096bac1baaf08f13a1534079b951
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-24 15:43:26 +0100

    responsive form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,89 +1,89 @@
-import "../App.css";
+import '../App.css'
 
-import { useState } from "react";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-library.add(faEye, faEyeSlash);
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { motion } from "framer-motion";
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+library.add(faEye, faEyeSlash)
 
 function App() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [p1, setP1] = useState("");
-	const [p2, setP2] = useState("");
-	const [submit, setSubmit] = useState(false);
-	const [alert, setAlert] = useState("");
-	const [hideP1, sethideP1] = useState(true);
-	const [hideP2, sethideP2] = useState(true);
-	const [shake, setShake] = useState(false);
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [p1, setP1] = useState('')
+	const [p2, setP2] = useState('')
 
-	const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
-		const value = event.target.value;
-		setName(value);
-	};
-	const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
-		const value = event.target.value;
-		setEmail(value);
-	};
-	const handleP1Change = (event: React.ChangeEvent<HTMLInputElement>):void => {
-		const value = event.target.value;
-		setP1(value);
-	};
-	const handleP2Change = (event: React.ChangeEvent<HTMLInputElement>):void => {
-		const value = event.target.value;
-		setP2(value);
-	};
-	const handleSubmit = (event: React.MouseEvent<HTMLFormElement>):void => {
-		event.preventDefault();
-		if (name === "") {
-			setAlert("name is require !");
-			setSubmit(false);
-		} else if (email === "") {
-			setAlert("email is require !");
-			setSubmit(false);
+	const [hideP1, sethideP1] = useState(true)
+	const [hideP2, sethideP2] = useState(true)
+	const [submit, setSubmit] = useState(false)
+	const [alert, setAlert] = useState('')
+	const [shake, setShake] = useState(false)
+
+	const handleNameChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		const value = event.target.value
+		setName(value)
+	}
+	const handleEmailChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		const value = event.target.value
+		setEmail(value)
+	}
+	const handleP1Change = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const value = event.target.value
+		setP1(value)
+	}
+	const handleP2Change = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const value = event.target.value
+		setP2(value)
+	}
+	const handleSubmit = (event: React.MouseEvent<HTMLFormElement>): void => {
+		event.preventDefault()
+		if (name === '') {
+			setAlert('name is require !')
+			setSubmit(false)
+		} else if (email === '') {
+			setAlert('email is require !')
+			setSubmit(false)
 		} else if (p1.length < 6) {
-			setAlert("7 charachers minimum !");
-			setSubmit(false);
-			setShake(true);
+			setAlert('7 charachers minimum !')
+			setSubmit(false)
+			setShake(true)
 			setTimeout(() => {
-				setShake(false);
-			}, 1000);
+				setShake(false)
+			}, 1000)
 		} else if (p1 !== p2) {
-			setAlert("passwords are not the same");
-			setSubmit(false);
-			setShake(true);
+			setAlert('passwords are not the same')
+			setSubmit(false)
+			setShake(true)
 			setTimeout(() => {
-				setShake(false);
-			}, 1000);
+				setShake(false)
+			}, 1000)
 		} else {
-			setAlert("");
-			setSubmit((prev) => !prev);
+			setAlert('')
+			setSubmit((prev) => !prev)
 		}
-	};
+	}
 	const handleHideP1 = () => {
-		sethideP1((prev) => !prev);
-	};
+		sethideP1((prev) => !prev)
+	}
 	const handleHideP2 = () => {
-		sethideP2((prev) => !prev);
-	};
-    const handleReturn = () => {
-        setSubmit(false);
-    }
+		sethideP2((prev) => !prev)
+	}
+	const handleReturn = () => {
+		setSubmit(false)
+	}
 	return (
 		<>
-		
-
 			<section className="flex justify-center leading-4 flex-row font-semibold w-full my-2 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl h-[80vh] items-center">
-				{name !== "" && email !== "" && p1.length > 6 && p1 === p2 && submit ? (
-					<motion.section 
+				{name !== '' && email !== '' && p1.length > 6 && p1 === p2 && submit ? (
+					<motion.section
 						animate={{ scale: submit ? 1 : 0 }}
 						initial={{ scale: 0.9 }}
-						transition={{ type: "spring", bounce: 0.6 }}
-						className="w-1/3 "
-						
+						transition={{ type: 'spring', bounce: 0.6 }}
+						className="max-md:w-2/3 md:w-1/3 "
 					>
 						<div className="my-2 flex flex-col bg-slate-200">
 							<label className="my-2">{name}</label>
@@ -102,9 +102,9 @@ function App() {
 					<motion.form
 						animate={{ scale: submit ? 0 : 1 }}
 						initial={{ scale: 0.9 }}
-						transition={{ type: "spring", bounce: 0.6 }}
+						transition={{ type: 'spring', bounce: 0.6 }}
 						onSubmit={handleSubmit}
-						className="flex flex-col my-2 w-1/3"
+						className="flex flex-col my-2 w-1/3 max-md:w-2/3"
 					>
 						<label className="my-2">Name</label>
 						<input
@@ -128,23 +128,22 @@ function App() {
 						<label className="my-2">Password</label>
 						<div className="relative ">
 							<motion.input
-								type={hideP1 ? "password" : "text"}
+								type={hideP1 ? 'password' : 'text'}
 								placeholder="azerty"
 								name="p1"
 								value={p1}
 								onChange={handleP1Change}
 								className={` ${
-									alert === "passwords are not the same" ||
-									alert === "7 charachers minimum !"
-										? "borderRed"
-										: ""
+									alert === 'passwords are not the same' ||
+									alert === '7 charachers minimum !'
+										? 'borderRed'
+										: ''
 								}  ${
-									shake ? "shake" : ""
+									shake ? 'shake' : ''
 								}  bg-slate-200 leading-4 border-none rounded w-full `}
-
 							/>
 							<FontAwesomeIcon
-								icon={hideP1 ? "eye" : "eye-slash"}
+								icon={hideP1 ? 'eye' : 'eye-slash'}
 								onClick={handleHideP1}
 								className="absolute top-1.5 right-2 "
 							/>
@@ -153,27 +152,29 @@ function App() {
 						<label className="my-2">Confirm your Password</label>
 						<div className="relative">
 							<input
-								type={hideP2 ? "password" : "text"}
+								type={hideP2 ? 'password' : 'text'}
 								placeholder="azerty"
 								name="p2"
 								value={p2}
 								onChange={handleP2Change}
 								className={` ${
-									alert === "passwords are not the same" ||
-									alert === "7 charachers minimum !"
-										? "borderRed"
-										: ""
+									alert === 'passwords are not the same' ||
+									alert === '7 charachers minimum !'
+										? 'borderRed'
+										: ''
 								}  ${
-									shake ? "shake" : ""
+									shake ? 'shake' : ''
 								}  bg-slate-200 leading-4 border-none rounded relative w-full `}
 							/>
 							<FontAwesomeIcon
-								icon={hideP2 ? "eye" : "eye-slash"}
+								icon={hideP2 ? 'eye' : 'eye-slash'}
 								onClick={handleHideP2}
 								className="absolute top-1.5 right-2 "
 							/>
 						</div>
-						<p className="text-red-500 my-2 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">{alert}</p>
+						<p className="text-red-500 my-2 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+							{alert}
+						</p>
 						<motion.button
 							whileTap={{ scale: 0.98 }}
 							className="my-8 border-none rounded bg-blue-vinted text-white "
@@ -183,9 +184,8 @@ function App() {
 					</motion.form>
 				)}
 			</section>
-			
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
