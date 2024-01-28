793c2cc94fa67a93a99c0d0cc83c2b5c9ef69d24
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-28 22:09:27 +0100

    save setToken

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
 type loginProps = {
-	layout: string;
+	layout: string
 	setToken: React.Dispatch<React.SetStateAction<string>>
 }
 
